a4b45deea51d395f2390f0aa8de0bc490c21c483
jrodriguesgcs <jrodrigues@globalcitizensolutions.com>
2025-09-22 11:47:15 +0100

    Update App.js
    Update Portugal Time to Citizenship informaton due to National Law being currently under revision - the Government intends to change it to 10 years.

--- src/App.js
+++ src/App.js
@@ -711,7 +711,7 @@ const ProgramComparisonTable = () => {
     "Portugal Golden Visa": {
       "PHYSICAL PRESENCE REQUIRED TO MAINTAIN STATUS": "7 days per year",
       "TIME TO PERMANENT RESIDENCY": "5 years",
-      "TIME TO CITIZENSHIP": "5 years",
+      "TIME TO CITIZENSHIP": "5 years - * National Law is currently under revision - the Government intends to change it to 10 years.",
       "PROCESSING PERIOD FROM SUBMISSION": "18-24 months",
       "MANDATORY VISIT TO THE COUNTRY": "Yes",
       "VISIT THE COUNTRY FOR BIOMETRICS": "Yes",
@@ -751,7 +751,7 @@ const ProgramComparisonTable = () => {
     "Portugal D7 Visa": {
       "PHYSICAL PRESENCE REQUIRED TO MAINTAIN STATUS": "183 days per year",
       "TIME TO PERMANENT RESIDENCY": "5 years",
-      "TIME TO CITIZENSHIP": "5 years",
+      "TIME TO CITIZENSHIP": "5 years - National Law is currently under revision - the Government intends to change it to 10 years.",
       "PROCESSING PERIOD FROM SUBMISSION": "3-4 months",
       "MANDATORY VISIT TO THE COUNTRY": "Yes",
       "VISIT THE COUNTRY FOR BIOMETRICS": "Yes",
